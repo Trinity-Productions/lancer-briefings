bfcc0e06d0aea74b4dbc57c3f0017b2729f61ed1
Trinity-Productions <98291891+Trinity-Productions@users.noreply.github.com>
2022-01-23 21:20:13 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -133,7 +133,7 @@ export default {
           "mech": "Valhalla"
         },
         {
-          "callsign": "JAHAJAH",
+          "callsign": "JAHJAH",
           "alias": "Khajiit",
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
