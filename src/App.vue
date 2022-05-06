ef7ae71d7d4e897cd5bb969d2765b2620449ef99
Trinity-Productions <98291891+Trinity-Productions@users.noreply.github.com>
2022-05-05 22:13:34 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -101,44 +101,28 @@ export default {
       ],
       "pilots": [
         {
-          "callsign": "RED",
-          "alias": "Frank Redenbacher",
-          "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "GMS",
-          "frame": "Sagarmatha",
-          "mech": "Family Business"
-        },
-        {
-          "callsign": "Yokai",
-          "alias": "Karitei Mo",
-          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Akuma"
-        },
-        {
           "callsign": "Queen",
           "alias": "Hollyann",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "B00M"
+          "corpro": "SSC",
+          "frame": "Monarch",
+          "mech": "Zenobia"
         },
         {
           "callsign": "Vanguard",
           "alias": "Ivan Daalgard",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Byrjun"
+          "corpro": "IPS-N",
+          "frame": "Blackbeard",
+          "mech": "Valhalla"
         },
         {
-          "callsign": "JAHJAH",
-          "alias": "Khajiit",
+          "callsign": "Ace",
+          "alias": "Faye Spiegel",
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Lil Milf"
+          "corpro": "IPS-N",
+          "frame": "Raliegh",
+          "mech": "Touch Me, But Not Too Much"
         },
       ],
       "header": {
